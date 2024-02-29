6754a3b5c50b3b6cd1775439bdab99bf5c24f593
ntutland <56928897+ntutland@users.noreply.github.com>
2024-02-29 09:07:59 -0500

    Apply black formatting to all python files

--- 1.LANDIS-MODEL/Driptorch_Support.py
+++ 1.LANDIS-MODEL/Driptorch_Support.py
@@ -10,128 +10,153 @@ import os
 import geopandas as gpd
 import pickle
 
-def write_ignitions(shape_path, ig_path, firing_dir, burn_path, epsg = 4326,
-                    rate = 1.5, fast = False, 
-                    line_type = "line", dash_length = None, gap_length = None,
-                    pattern = "strip", crew_size = 8, depth = 60, offset = 20):
+
+def write_ignitions(
+    shape_path,
+    ig_path,
+    firing_dir,
+    burn_path,
+    epsg=4326,
+    rate=1.5,
+    fast=False,
+    line_type="line",
+    dash_length=None,
+    gap_length=None,
+    pattern="strip",
+    crew_size=8,
+    depth=60,
+    offset=20,
+):
     burn_plot = gpd.read_file(shape_path)
     burn_domain = gpd.read_file(burn_path)
-    firing_area = setup_burnunit(burn_plot,firing_dir,epsg)
-    burn_domain = setup_burndomain(burn_domain,firing_dir,epsg = epsg)
-    igniter = create_igniters(rate,fast,line_type,dash_length,gap_length)
+    firing_area = setup_burnunit(burn_plot, firing_dir, epsg)
+    burn_domain = setup_burndomain(burn_domain, firing_dir, epsg=epsg)
+    igniter = create_igniters(rate, fast, line_type, dash_length, gap_length)
     crew = setup_crew(igniter, crew_size)
     ig_pattern = setup_ignitions(firing_area, crew, pattern, depth, offset)
-    ig_pattern.to_quicfire(burn_domain,filename=os.path.join(ig_path,"ignite.dat"),dst_epsg=epsg,resolution=2)
+    ig_pattern.to_quicfire(
+        burn_domain,
+        filename=os.path.join(ig_path, "ignite.dat"),
+        dst_epsg=epsg,
+        resolution=2,
+    )
     delete_ignite_header(ig_path)
     if line_type == "dot":
         delete_ignite_zeros(ig_path)
-    ig_pickle = open(os.path.join(ig_path,'ignite.obj'), 'wb') 
+    ig_pickle = open(os.path.join(ig_path, "ignite.obj"), "wb")
     pickle.dump(ig_pattern, ig_pickle)
     ig_pickle.close()
     how_long(ig_pattern)
 
-def setup_burnunit(burn_plot,firing_dir, epsg):
-    burn_unit = dt.BurnUnit(burn_plot.iloc[0,-1], firing_dir, utm_epsg = epsg)
+
+def setup_burnunit(burn_plot, firing_dir, epsg):
+    burn_unit = dt.BurnUnit(burn_plot.iloc[0, -1], firing_dir, utm_epsg=epsg)
     firing_area = burn_unit.buffer_control_line(10)
     return firing_area
 
+
 def setup_burndomain(burn_domain, firing_dir, epsg):
-    burn_domain = dt.BurnUnit(burn_domain.iloc[0,-1], firing_dir, utm_epsg = epsg)
+    burn_domain = dt.BurnUnit(burn_domain.iloc[0, -1], firing_dir, utm_epsg=epsg)
     return burn_domain
 
+
 def create_igniters(rate, fast, line_type, dash_length, gap_length):
-    fastrate = 10 # m/s
+    fastrate = 10  # m/s
     if fast:
-        rate = fastrate 
+        rate = fastrate
     if line_type == "line":
         igniter = dt.Igniter(rate)
     elif line_type == "dash":
         igniter = dt.Igniter(rate, dash_length, gap_length)
     elif line_type == "dot":
-        igniter = dt.Igniter(rate, gap_length = gap_length)
+        igniter = dt.Igniter(rate, gap_length=gap_length)
     else:
         print("Unsupported ignition line type")
-    
+
     return igniter
+
 
 def setup_crew(igniter, size):
     crew = dt.IgnitionCrew.clone_igniter(igniter, size)
     return crew
 
-def setup_ignitions(burn_unit, crew, pattern, depth = 60, offset = 20):
+
+def setup_ignitions(burn_unit, crew, pattern, depth=60, offset=20):
     if pattern == "strip":
         ignitions = dt.firing.Strip(burn_unit, crew)
-        ig_pattern = ignitions.generate_pattern(spacing = 0, depth = depth)
+        ig_pattern = ignitions.generate_pattern(spacing=0, depth=depth)
     elif pattern == "flank":
         ignitions = dt.firing.Flank(burn_unit, crew)
-        ig_pattern = ignitions.generate_pattern(depth = depth)
+        ig_pattern = ignitions.generate_pattern(depth=depth)
     elif pattern == "head":
         ignitions = dt.firing.Head(burn_unit, crew)
-        ig_pattern = ignitions.generate_pattern(offset = offset)
+        ig_pattern = ignitions.generate_pattern(offset=offset)
     elif pattern == "back":
         ignitions = dt.firing.Back(burn_unit, crew)
-        ig_pattern = ignitions.generate_pattern(offset = offset)
+        ig_pattern = ignitions.generate_pattern(offset=offset)
     elif pattern == "ring":
         ignitions = dt.firing.Ring(burn_unit, crew)
-        ig_pattern = ignitions.generate_pattern(offset = offset)
+        ig_pattern = ignitions.generate_pattern(offset=offset)
     else:
         print("Unsupported ignition pattern")
-    
+
     return ig_pattern
-    
+
+
 def delete_ignite_header(path):
-    file = os.path.join(path,"ignite.dat")
+    file = os.path.join(path, "ignite.dat")
     # list to store file lines
     lines = []
     # read file
-    with open(file, 'r') as fp:
+    with open(file, "r") as fp:
         # read an store all lines into list
         lines = fp.readlines()
     # Write file
-    with open(file, 'w') as fp:
+    with open(file, "w") as fp:
         # iterate each line
         for number, line in enumerate(lines):
             if number not in range(7):
                 fp.write(line)
 
+
 def delete_ignite_zeros(path):
-    file = os.path.join(path,"ignite.dat")
+    file = os.path.join(path, "ignite.dat")
     # list to store file lines
     lines = []
     # read file
-    with open(file, 'r') as fp:
+    with open(file, "r") as fp:
         # read an store all lines into list
         lines = fp.readlines()
-        linecount = len(lines) - 6 # first 6 lines aren't ignitions
+        linecount = len(lines) - 6  # first 6 lines aren't ignitions
 
     deleted = 0
     # Write file
-    with open(file, 'w') as fp:
+    with open(file, "w") as fp:
         # iterate each line
         for line in lines:
-            if line.startswith('0'):
+            if line.startswith("0"):
                 deleted += 1
             else:
                 fp.write(line)
 
-    with open(file, 'r') as fp:
+    with open(file, "r") as fp:
         # read an store all lines into list
         lines = fp.readlines()
 
     # Write file
-    with open(file, 'w') as fp:
+    with open(file, "w") as fp:
         # iterate each line
         for line in lines:
-            if line.find (' 0 ') != -1:
+            if line.find(" 0 ") != -1:
                 deleted += 1
             else:
                 fp.write(line)
 
-    newlinecount = linecount - deleted 
-    with open(file, 'r') as fp:
+    newlinecount = linecount - deleted
+    with open(file, "r") as fp:
         # read an store all lines into list
         lines = fp.readlines()
-    with open(file, 'w') as fp:
+    with open(file, "w") as fp:
         for line in lines:
             if line.startswith("naerial="):
                 fp.write("naerial={}\n".format(newlinecount))
@@ -143,17 +168,13 @@ def how_long(ignitions):
     t = ignitions.elapsed_time
     print("Ignitions take {} seconds".format(t))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     OG_PATH = os.getcwd()
-    shape_path = os.path.join(OG_PATH,"Shapefiles","burn_plot.shp")
-    ig_path = os.path.join(OG_PATH,"Ignitions")
+    shape_path = os.path.join(OG_PATH, "Shapefiles", "burn_plot.shp")
+    ig_path = os.path.join(OG_PATH, "Ignitions")
     firing_dir = 270
-    
-    write_ignitions(shape_path,ig_path,firing_dir,line_type = "dash",dash_freq = 10,pattern="head")
-    
-    
-    
-    
-    
-    
-    
+
+    write_ignitions(
+        shape_path, ig_path, firing_dir, line_type="dash", dash_freq=10, pattern="head"
+    )
--- 1.LANDIS-MODEL/Track_Fuels.py
+++ 1.LANDIS-MODEL/Track_Fuels.py
@@ -14,72 +14,73 @@ import os
 import numpy as np
 import pandas as pd
 
+
 class FuelsTracker:
     def __init__(self, VDM_folder, lp):
         os.chdir("..")
         OG_PATH = os.getcwd()
         os.chdir(VDM_folder)
-        self.scorch_path = os.path.join(OG_PATH,"8.CROWN-SCORCH")
-        
+        self.scorch_path = os.path.join(OG_PATH, "8.CROWN-SCORCH")
+
         self.tree_tracker = "TreeTracker.txt"
         self.percent_fuel = "PercentFuelChange.txt"
         self.treelist = "treelist_VDM.dat"
         self.grass_fuel = "VDM_litter_WG.dat"
         self.litter_fuel = "VDM_litter_trees.dat"
-        
+
         self.VDM_folder = VDM_folder
         self.out_path = os.path.join(OG_PATH, VDM_folder, "FM2VDM")
         os.makedirs(self.out_path, exist_ok=True)
-        
+
         self.trees_out = "AfterFireTrees.txt"
         self.grass_out = "AfterFireWG.txt"
         self.litter_out = "AfterFireLitter.txt"
-        
+
         file_list = [
             self.tree_tracker,
             self.percent_fuel,
             self.treelist,
             self.grass_fuel,
-            self.litter_fuel
-            ]
-        
+            self.litter_fuel,
+        ]
+
         self.filelist = []
         for i in file_list:
-            if os.path.exists(os.path.join(self.scorch_path,i)):
+            if os.path.exists(os.path.join(self.scorch_path, i)):
                 self.filelist.append(i)
-        
+
         # Decide threshold for tree death by species
         self.threshold_dict = get_thresholds(lp)
-                
 
-def postfire_fuels(ft,tp,lp):
+
+def postfire_fuels(ft, tp, lp):
     ## ft = FuelsTracker class
     ## tp = Treelist_params class
     ## lp = Landis_params class
-    
+
     ## Set up empty arrays
-        # tree and surface, 1-D and n-D
+    # tree and surface, 1-D and n-D
     # tree_arr = np.zeros((tp.nx,tp.ny,tp.nz))
     # tree_arr_1d = tree_arr.flatten()
     # surf_arr = np.zeros((tp.nx,tp.ny))
     # surf_arr_1d = surf_arr.flatten()
-    
+
     # DEFINE DOMAIN
     Nx = tp.nx
     Ny = tp.ny
     Nz = tp.nz
-    s = (Nx,Ny)
-    array1d = Nx*Ny*Nz
+    s = (Nx, Ny)
+    array1d = Nx * Ny * Nz
     percentFuelChang1d = np.zeros(array1d)
     postfire_fuel = np.zeros(s)
-    
+
     ## Import percent change in fuels for each cell
-    with open(os.path.join(ft.scorch_path,ft.percent_fuel), 'r') as pfc:
+    with open(os.path.join(ft.scorch_path, ft.percent_fuel), "r") as pfc:
         count = 0
         for line in pfc.readlines():
             percentFuelChang1d[count] = float(line)
             count = count + 1
-            
+
     # ## Import percent change in fuels for each cell
     # pfc_2d = np.loadtxt(os.path.join(ft.scorch_path, ft.percent_fuel))
     # pfc_3d = pfc_2d.reshape(
@@ -90,76 +91,93 @@ def postfire_fuels(ft,tp,lp):
     # surface_change = pfc_3d[:,:0]
     # canopy_change = pfc_3d.copy()
 
-    
     ## Calculate change in surface fuels
-    for i in [ft.grass_fuel,ft.litter_fuel]:
+    for i in [ft.grass_fuel, ft.litter_fuel]:
         if i in ft.filelist:
-            with open(os.path.join(ft.scorch_path,i), 'r') as pff:
+            with open(os.path.join(ft.scorch_path, i), "r") as pff:
                 prefire_fuel = pff.read().split()
             for locy in range(Ny):
                 for locx in range(Nx):
                     gloc = locx + (locy * Nx)
-                    planarloc = (Nx*(Ny-1)-(locy*Nx)) + locx
-                    postfire_fuel[locx,locy] = float(prefire_fuel[gloc]) * percentFuelChang1d[planarloc]
-            if i==ft.grass_fuel:
-                np.savetxt(os.path.join(ft.out_path,ft.grass_out), postfire_fuel, fmt='%10.2f')
-            elif i==ft.litter_fuel:
-                np.savetxt(os.path.join(ft.out_path,ft.litter_out), postfire_fuel, fmt='%10.2f')
-    
+                    planarloc = (Nx * (Ny - 1) - (locy * Nx)) + locx
+                    postfire_fuel[locx, locy] = (
+                        float(prefire_fuel[gloc]) * percentFuelChang1d[planarloc]
+                    )
+            if i == ft.grass_fuel:
+                np.savetxt(
+                    os.path.join(ft.out_path, ft.grass_out), postfire_fuel, fmt="%10.2f"
+                )
+            elif i == ft.litter_fuel:
+                np.savetxt(
+                    os.path.join(ft.out_path, ft.litter_out),
+                    postfire_fuel,
+                    fmt="%10.2f",
+                )
+
     ## Which trees die?
-    with open(os.path.join(ft.out_path,ft.trees_out), 'w') as aft:
-        with open(os.path.join(ft.scorch_path,ft.tree_tracker), 'r') as tt:
-            with open(os.path.join(ft.scorch_path,ft.treelist), 'r') as tl:
-                treezip = zip(tt,tl)
+    with open(os.path.join(ft.out_path, ft.trees_out), "w") as aft:
+        with open(os.path.join(ft.scorch_path, ft.tree_tracker), "r") as tt:
+            with open(os.path.join(ft.scorch_path, ft.treelist), "r") as tl:
+                treezip = zip(tt, tl)
                 livetrees = 0
                 deadtrees = 0
                 for tt, tl in treezip:
                     line_tt = tt.split()
                     line_tl = tl.split()
-                    cellnum = int(line_tt[1]) #number of cells with fuel from that tree
+                    cellnum = int(
+                        line_tt[1]
+                    )  # number of cells with fuel from that tree
                     totfuel = 0.0
-                    sppflag = int(line_tl[0]) #tree species identifier
+                    sppflag = int(line_tl[0])  # tree species identifier
                     threshold = ft.threshold_dict[sppflag]
                     for cell in range(cellnum):
-                        cell_index = int(line_tt[2+cell]) #first two items in list are tree id and cellnum, so cound over starting on third item
-                        fuel_conc = float(line_tt[2+cell+cellnum]) #next items correspond to inital fuel density in each cell
+                        cell_index = int(
+                            line_tt[2 + cell]
+                        )  # first two items in list are tree id and cellnum, so cound over starting on third item
+                        fuel_conc = float(
+                            line_tt[2 + cell + cellnum]
+                        )  # next items correspond to inital fuel density in each cell
                         newfuel = percentFuelChang1d[cell_index] * fuel_conc
-                        totfuel = totfuel + newfuel # sum the total amount of remaininf fuel for that tree
+                        totfuel = (
+                            totfuel + newfuel
+                        )  # sum the total amount of remaininf fuel for that tree
                     if totfuel > threshold:
                         aft.write(tl)
                         livetrees += 1
                     else:
                         deadtrees += 1
-    LiveDeadList=[]
+    LiveDeadList = []
     LiveDeadList.append(livetrees)
     LiveDeadList.append(deadtrees)
-    
+
     return LiveDeadList
-                  
+
 
 def get_thresholds(lp):
     # mortality threshold dict from LANDIS_options
-    threshold_dict = dict(zip(lp.fia_spec,lp.mortality_thresholds))
+    threshold_dict = dict(zip(lp.fia_spec, lp.mortality_thresholds))
     # Species IDs to species names from treelist
-    treelist = pd.read_csv(os.path.join(lp.landis_path,"Treelist_postlandis_cycle"+str(lp.cycle)+".csv"))
-    spids = list(range(1,len(treelist["SPID"].unique())+1,1))
+    treelist = pd.read_csv(
+        os.path.join(
+            lp.landis_path, "Treelist_postlandis_cycle" + str(lp.cycle) + ".csv"
+        )
+    )
+    spids = list(range(1, len(treelist["SPID"].unique()) + 1, 1))
     sps = list(treelist["SPECIES_SYMBOL"].unique())
-    spid_dict = dict(zip(sps,spids))
+    spid_dict = dict(zip(sps, spids))
     # Match species ID to threshold by species name
-    dicts = [threshold_dict,spid_dict]
+    dicts = [threshold_dict, spid_dict]
     merge = merge_dictionary_list(dicts)
     mortality_dict = {}
     for i in merge.values():
-        if len(i)==2:
+        if len(i) == 2:
             mortality_dict[i[1]] = i[0]
 
     return mortality_dict
 
 
 def merge_dictionary_list(dict_list):
-  return {
-    k: [d.get(k) for d in dict_list if k in d] # explanation A
-    for k in set().union(*dict_list) # explanation B
-  }
-
-
+    return {
+        k: [d.get(k) for d in dict_list if k in d]  # explanation A
+        for k in set().union(*dict_list)  # explanation B
+    }
